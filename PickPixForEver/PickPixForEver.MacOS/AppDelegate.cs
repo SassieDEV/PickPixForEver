a84b73d3552f9aeec656f8f427fbc3ae1a488dd0
Saleh Sultan <>
2020-03-28 11:34:41 -0500

    Added Mac Project. There are some issues, but up to this point, everything is working.

---
+++ PickPixForEver/PickPixForEver.MacOS/AppDelegate.cs
@@ -0,0 +1,48 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace PickPixForEver.MacOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        NSWindow window;
+
+        public override NSWindow MainWindow => window;
+
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+            var rect = new CoreGraphics.CGRect(100, 100, 1024, 768);
+
+            window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            window.Title = "PickPixForEver";
+            window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+
+            //SQLitePCL.Batteries.Init();
+            //var libPath = Path.Combine(
+            //    Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            //    , "..", "Library", "data");
+            //if (!Directory.Exists(libPath))
+            //{
+            //    Directory.CreateDirectory(libPath);
+            //}
+            //var dbPath = Path.Combine(libPath, "PickPixForever.db");
+
+            LoadApplication(new App(""));
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
