c27650ff1c43aaec5f3519a5a368a95d7ca913c4
Gabriel Grinffiel <gabegamergrinff@gmail.com>
2020-03-29 14:01:49 -0500

    Adding Picture Repository and Interface
    Still working on EnterPicture, just wanted to get a commit in place

---
+++ PickPixForEver/PickPixForEver/Services/IPictureRepository.cs
@@ -0,0 +1,14 @@
+﻿using PickPixForEver.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickPixForEver.Services
+{
+    public interface IPictureRepository
+    {
+        Task<int> EnterPicture(Picture picture);
+        Task<Picture> GetPicture(int ID);
+    }
+}
---
+++ PickPixForEver/PickPixForEver/Services/PictureRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PickPixForEver.Models;
+using PickPixForEver.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickPixForEver.Services
+{
+    public class PictureRepository : IPictureRepository
+    {
+        private readonly string filePath;
+
+        public PictureRepository(string filePath)
+        {
+            this.filePath = filePath;
+
+        }
+        public async Task<int> EnterPicture(Picture picture)
+        {
+            return 1;
+        }
+        public async Task<Picture> GetPicture(int ID)
+        {
+            Picture pic;
+
+            using (var dbContext = new PickPixDbContext(filePath)) {
+                var pic1 = dbContext.Pictures.Select(s => s).FirstOrDefault();
+                pic = await dbContext.Pictures.
+                Where(s => s.Id == ID).SingleOrDefaultAsync().ConfigureAwait(false);
+                return pic1;
+            }
+        }
+    }
+}
