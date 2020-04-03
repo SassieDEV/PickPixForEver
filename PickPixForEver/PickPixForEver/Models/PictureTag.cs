4d7ac0c7cffff14356fbad56d23118f6fa157d09
Gabriel Grinffiel <gxg141830@utdallas.edu>
2020-04-02 23:05:30 -0500

    Update PictureTag.cs
    Removed PictureId as I don't believe you should be able to go from 1Tag ->1 picture, TagId removed as it is redundant

--- PickPixForEver/PickPixForEver/Models/PictureTag.cs
+++ PickPixForEver/PickPixForEver/Models/PictureTag.cs
@@ -7,9 +7,6 @@ namespace PickPixForEver.Models
     public class PictureTag
     {
         public int PictureTagId { get; set; }
-        public int PictureId { get; set; }
-        public int TagId { get; set; }
-        //public Picture Picture { get; set; }
-       // public Tag Tag { get; set; }
+        public string TagDescriptor { get; set; }
     }
 }
