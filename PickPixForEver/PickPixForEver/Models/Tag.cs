c2d28488d4fb21d84b679779c977bd9b441c5d6e
Gabriel Grinffiel <gabegamergrinff@gmail.com>
2020-04-04 11:57:00 -0500

    Removed Name from Tag, now is just Value and TagType

--- PickPixForEver/PickPixForEver/Models/Tag.cs
+++ PickPixForEver/PickPixForEver/Models/Tag.cs
@@ -7,7 +7,6 @@ namespace PickPixForEver.Models
     public class Tag
     {
         public int TagId { get; set; }
-        public string Name { get; set; }
         public string Value { get; set; }
         public string TagType { get; set; }
         public DateTime Created { get; set; }
